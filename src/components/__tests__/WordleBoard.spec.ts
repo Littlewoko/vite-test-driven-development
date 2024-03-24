b01719a6349d69a761142e9cbe3d511985b302e9
Bradley Watkins <watkinsbradley01@gmail.com>
2024-03-24 19:32:57 +0000

    defeat message test

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE } from "@/settings";
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from "@/settings";
 
 describe('WorldeBoard', () => {
   test("A victory message appears when the user makes a guess that matches the word of the day", async () => {
@@ -15,4 +15,16 @@ describe('WorldeBoard', () => {
     // Assert
     expect(wrapper.text()).toContain(VICTORY_MESSAGE);
   })
+
+  test("a defeat message appears if the user makes a guess that is incorrect", async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } })
+
+    const guessInput = wrapper.find("input[type=text]");
+    await guessInput.setValue("WRONG");
+    guessInput.trigger("keydown.enter");
+
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE);
+  })
+
+  test.todo("no end-of-game message appears if the user has not yet made a guess");
 })
--- src/settings.ts
+++ src/settings.ts
@@ -1,1 +1,2 @@
 export const VICTORY_MESSAGE = "you won!";
+export const DEFEAT_MESSAGE = "you lose!";
