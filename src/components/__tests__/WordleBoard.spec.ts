9a3197f357dba672a5f52754664b8db68d031521
Bradley Watkins <watkinsbradley01@gmail.com>
2024-03-24 19:51:31 +0000

    extract arrange phase into beforeeach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,26 +1,24 @@
-import { mount } from '@vue/test-utils'
+import { VueWrapper, mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from "@/settings";
 
 describe('WorldeBoard', () => {
   const wordOfTheDay = "TESTS";
 
-  test("A victory message appears when the user makes a guess that matches the word of the day", async () => {
-    // Arrage
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  let wrapper : ReturnType<typeof mount>;
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay } }) 
+  })
 
-    // Act
+  test("A victory message appears when the user makes a guess that matches the word of the day", async () => {
     const guessInput = wrapper.find("input[type=text]");
     await guessInput.setValue("TESTS");
     await guessInput.trigger("keydown.enter");
 
-    // Assert
     expect(wrapper.text()).toContain(VICTORY_MESSAGE);
   })
 
   test("a defeat message appears if the user makes a guess that is incorrect", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
-
     const guessInput = wrapper.find("input[type=text]");
     await guessInput.setValue("WRONG");
     await guessInput.trigger("keydown.enter");
