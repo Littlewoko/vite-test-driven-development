3de3fe7e45bf517df9af013b9d471f1c1363adb6
Bradley Watkins <watkinsbradley01@gmail.com>
2024-03-26 22:44:21 +0000

    Randopm word of the day

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+import englishWords from "@/englishWordsWith5Letters.json";
+
 import WordleBoard from "./components/WordleBoard.vue";
+
+const wordOfTheDay = ref(englishWords[Math.floor(Math.random() * englishWords.length)]);
 </script>
 
 <template>
-  <WordleBoard word-of-the-day="FIVES" />
+  <WordleBoard :word-of-the-day="wordOfTheDay.toUpperCase()" />
 </template>
 
 
